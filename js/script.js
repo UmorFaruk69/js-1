1c10db973a88ce90f752cf94a2b67c3c2123a120
Umor Faruk <101555985+UmorFaruk69@users.noreply.github.com>
2024-08-07 11:26:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Example-1
+function showText1() {
+    document.getElementById('s_btn_1').innerHTML =
+      "Hello World!";
+}
+
+// Example-2
+function showText2() {
+    document.getElementById('s_btn_2').innerHTML = "Hello World!";
+}
+
+// Example-3
+function showText3() {
+    document.getElementById('s_btn_3').innerHTML = "Hello World!";
+}
+
+// Example 4
+function showText4() {
+    document.getElementById('s_btn_4').innerHTML = "Hello World!";
+}
+
+// Example 5
+function showText5() {
+    document.getElementById('s_btn_5').innerHTML = "Hello World!";
+}
+
+// Example-6
+function showText6() {
+    document.getElementById('s_btn_6').innerHTML =
+      "Hello World!";
+}
+
+// Example-7
+function showText7() {
+    document.getElementById('s_btn_7').innerHTML = "Hello World!";
+}
+
+// Example-8
+function showText8() {
+    document.getElementById('s_btn_8').innerHTML = "Hello World!";
+}
+
+// Example-9
+function showText9() {
+    document.getElementById('s_btn_9').innerHTML = "Hello World!";
+}
+
+// Example-10
+function showText10() {
+    document.getElementById('s_btn_10').innerHTML = "Hello World!";
+}
